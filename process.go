2cb48967abd47217b5c5afd126ab8369b2636037
will <will@dropbox.com>
2013-08-06 18:12:48 -0700

    modify main() to send PCM data to standard output

--- process.go
+++ process.go
@@ -1,25 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
 	"log"
+	"os"
 	"github.com/bigwill/process/core"
 	"github.com/bigwill/process/lib/generator/square"
 	"github.com/bigwill/process/lib/processor/gain"
 	"github.com/bigwill/process/lib/processor/rcfilter"
 )
 
+const bufferSize = 500
+
 func main() {
 	var square core.Generator = square.NewGenerator(48000.0)
 	var filter core.Processor = rcfilter.NewProcessor(48000.0)
 	var gain core.Processor = gain.NewProcessor(48000.0)
 	filter.Param(0).SetPos(.2)
-	square.Param(0).SetPos(.6)
+	square.Param(0).SetPos(.1)
+	gain.Param(0).SetPos(.5)
 
 	log.Printf("%s %s", square.Name(), square.Param(0).Repr())
 	log.Printf("%s %s", filter.Name(), filter.Param(0).Repr())
 
-	for i := 0; i < 4800; i++ {
-		fmt.Printf("%v\t%v\n", i, gain.Process(filter.Process(square.Generate())))
+	buf := make([]core.Quantity, bufferSize, bufferSize)
+	for {
+		for i := 0; i < bufferSize; i++ {
+			buf[i] = gain.Process(filter.Process(square.Generate()))
+		}
+		err := binary.Write(os.Stdout, binary.LittleEndian, buf)
+		if err != nil {
+			log.Printf("%v", err)
+			return
+		}
 	}
 }
